9239e777304be83588f8ff98bfe0224c4f9c665c
Harsh Trivedi <harshtrivedi74@gmail.com>
2022-02-06 12:03:22 +0530

    Dependency Injection Setup

--- app/src/main/java/com/harsh/notes/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/harsh/notes/feature_note/domain/use_case/NoteUseCases.kt
@@ -2,5 +2,5 @@ package com.harsh.notes.feature_note.domain.use_case
 
 data class NoteUseCases(
     val getNotes: GetNotesUseCase,
-    val deleteNotes: DeleteNoteUseCase
+    val deleteNote: DeleteNoteUseCase
 )
