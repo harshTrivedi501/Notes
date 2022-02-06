1d606c0bd005aaef3b3db0422c8bdaa31f021b14
Harsh Trivedi <harshtrivedi74@gmail.com>
2022-02-06 11:01:55 +0530

    Project Setup

---
+++ app/src/main/java/com/harsh/notes/feature_note/presentation/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.harsh.notes.feature_note.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.harsh.notes.ui.theme.DarkGray
+import com.harsh.notes.ui.theme.NotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesTheme {
+
+            }
+        }
+    }
+}
